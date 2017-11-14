8fb16409a3ea37d40ebb74f0f21b903d53266901
SbWereWolf <ulfnew@gmail.com>
2017-11-14 22:29:27 +0500

    bug fix
    был глупый баг - в выборку включались записи у которых показатели не выбегали за 5% допуск, а по заданию необходимо показать записи которые напротив - выбегают за 5% от среднего измерения за предыдущий день
    было mdc.avg_value BETWEEN mdp.avg_value * 0.95 AND mdp.avg_value * 1.05
    а надо mdc.avg_value NOT BETWEEN mdp.avg_value * mrr.minimum AND mdp.avg_value * mrr.maximum

--- analysis_of_measure.sql
+++ analysis_of_measure.sql
@@ -10,28 +10,43 @@ CREATE TABLE log AS
   FROM generate_series(1, 666);
 
 /* perform analysis of measure data */
-WITH mesuare_dates (ts, device,avg_value) AS (
+WITH mesuare_dates (ts, device, avg_value) AS (
     SELECT
-      date_trunc('day', l.ts),
-      l.device_id,
-      AVG(l.value)
+      date_trunc('day', l.ts) ts,
+      l.device_id             device,
+      AVG(l.value)            avg_value
     FROM
       log l
     GROUP BY
-      date_trunc('day', l.ts),l.device_id
+      date_trunc('day', l.ts), l.device_id
 )
-  , target_dates (ts,device) AS (
-    SELECT mdc.ts,mdc.device
-
+  , variation_tolerance ( variation ) AS (
+    SELECT 0.05
+)
+  , mesuare_range_ratio (minimum,maximum) AS (
+    SELECT
+      1 - vt.variation minimum,
+      1 + vt.variation maximum
+    FROM variation_tolerance vt
+)
+  , target_dates (ts, device) AS (
+    SELECT
+      mdc.ts,
+      mdc.device
     FROM
+      mesuare_range_ratio mrr,
       mesuare_dates mdc
-      join mesuare_dates mdp
-      on mdc.device = mdp.device
+      JOIN mesuare_dates mdp
+        ON mdc.device = mdp.device
     WHERE
       mdc.ts = mdp.ts + INTERVAL '1 day'
-      AND mdc.avg_value BETWEEN mdp.avg_value * 0.95 AND mdp.avg_value * 1.05
+      AND mdc.avg_value NOT BETWEEN mdp.avg_value * mrr.minimum AND mdp.avg_value * mrr.maximum
 )
-SELECT td.ts,td.device, l.ts,l.value
+SELECT
+  td.ts     date,
+  td.device device,
+  l.ts      mesuare_timestamp,
+  l.value   mesuare_value
 FROM
   target_dates td,
   log l
@@ -39,4 +54,4 @@ WHERE
   l.ts >= td.ts
   AND l.ts < td.ts + INTERVAL '1 day'
   AND l.device_id = td.device
-ORDER BY td.ts, l.device_id, l.ts;
+ORDER BY l.device_id, td.ts, l.ts;
